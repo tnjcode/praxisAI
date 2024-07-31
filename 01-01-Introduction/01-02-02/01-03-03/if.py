4cc0608fe35f1e4e568e892853a445b0b57a6644
tnjcode <fachrialkayyis@gmail.com>
2024-07-31 13:52:59 +0700

    Co-authored-by: KurungAwal <KurungAwal@users.noreply.github.com>

--- 01-01-Introduction/01-02-02/01-03-03/if.py
+++ 01-01-Introduction/01-02-02/01-03-03/if.py
@@ -1,9 +1,10 @@
 user =  "ALI"
-
+user2 =  "fachri"
 user_in = input("input the user name")
 
 if user_in == user:
     print("welcome to this app")
-
+elif user_in == user2:
+    print("welcome")
 else:
     print("you are a stranger")
---
+++ 01-01-Introduction/01-02-02/01-03-03/whileloop.py
@@ -0,0 +1,4 @@
+i = 0
+while True<1000:
+    print("fattan siomay")
+    i+=1
